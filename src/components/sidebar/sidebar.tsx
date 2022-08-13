923a2b0b075eef696467bdce5009a3fc96af2259
Omar Baldi <omarbaldidk@gmail.com>
2022-08-13 22:43:02 +0200

    Wrapper navigation menu list around NavLink react router dom to either detect being active or not and applied active style

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -1,31 +1,30 @@
 import { FC } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import SidebarProps from './dto';
 import Styles from './sidebar.module.scss';
 
-/**
- * TODO: add dynamic class active if current URL match the link URL
- */
 const Sidebar: FC<SidebarProps> = ({
   links,
   additionalContent,
 }: SidebarProps) => {
-  const navigate = useNavigate();
-
   return (
     <aside className={Styles.sidebarWrapper}>
       <nav className={Styles.sidebar}>
         <ul className={Styles.linksWrapper}>
           {links.map(({ url, label, icon: IconElement }, index: number) => {
             return (
-              <li
-                key={`nav-link-#${index}`}
-                className={Styles.link}
-                onClick={() => navigate(url)}
-              >
-                <span className={Styles.iconWrapper}>{IconElement}</span>
-                <span className={Styles.linkLabel}>{label}</span>
-              </li>
+              <NavLink to={url} key={`nav-link-#${index}`}>
+                {({ isActive }) => (
+                  <li
+                    className={`${Styles.link} ${
+                      isActive ? Styles.activeLink : ''
+                    }`}
+                  >
+                    <span className={Styles.iconWrapper}>{IconElement}</span>
+                    <span className={Styles.linkLabel}>{label}</span>
+                  </li>
+                )}
+              </NavLink>
             );
           })}
         </ul>
