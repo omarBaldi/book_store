6d674ddf215c40c5a53d934e58e8c7b2327a8f72
Omar Baldi <omarbaldidk@gmail.com>
2022-08-13 12:04:56 +0200

    Implemented use of useNavigate hook from react router dom to navigate to specific url as soon as card gets clicked

--- src/components/category-card/category-card.tsx
+++ src/components/category-card/category-card.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react';
 import CategoryCardProps from './dto';
 import Styles from './category-card.module.scss';
+import { useNavigate } from 'react-router-dom';
 
 const CategoryCard: FC<CategoryCardProps> = ({
   title,
@@ -8,8 +9,15 @@ const CategoryCard: FC<CategoryCardProps> = ({
   totalItemsLabel = 'items',
   additionalStyle = {},
 }: CategoryCardProps) => {
+  const navigate = useNavigate();
+  const handleCardClick = (): void => navigate(`/categories/${title}`);
+
   return (
-    <div className={Styles.categoryCardWrapper} style={{ ...additionalStyle }}>
+    <div
+      className={Styles.categoryCardWrapper}
+      style={{ ...additionalStyle }}
+      onClick={handleCardClick}
+    >
       <div>
         <h1>{title}</h1>
         <p>
