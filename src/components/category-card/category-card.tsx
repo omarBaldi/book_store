ff8760016d9363bbe308f839fb7542798705c42e
Omar Baldi <omarbaldidk@gmail.com>
2022-08-13 12:28:33 +0200

    Applied new style for category card component

--- src/components/category-card/category-card.tsx
+++ src/components/category-card/category-card.tsx
@@ -1,30 +1,34 @@
 import { FC } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { CircleIcon } from '../../assets/icons';
 import CategoryCardProps from './dto';
 import Styles from './category-card.module.scss';
-import { useNavigate } from 'react-router-dom';
+import { Button } from '../button';
+import { BUTTON_SIZES } from '../button/dto';
 
 const CategoryCard: FC<CategoryCardProps> = ({
   title,
   totalItems,
   totalItemsLabel = 'items',
+  buttonLabel = 'Discover more',
   additionalStyle = {},
 }: CategoryCardProps) => {
   const navigate = useNavigate();
   const handleCardClick = (): void => navigate(`/categories/${title}`);
 
   return (
-    <div
-      className={Styles.categoryCardWrapper}
-      style={{ ...additionalStyle }}
-      onClick={handleCardClick}
-    >
+    <div className={Styles.categoryCardWrapper} style={{ ...additionalStyle }}>
       <div>
-        <h1>{title}</h1>
-        <p>
-          <span>{totalItems}</span>
-          <span>{totalItemsLabel}</span>
-        </p>
+        <CircleIcon className={Styles.icon} />
+        <h2 className={Styles.cardTitle}>{title}</h2>
+        <p
+          className={Styles.itemsLabel}
+        >{`${totalItems} ${totalItemsLabel}`}</p>
       </div>
+
+      <Button cbFunc={handleCardClick} size={BUTTON_SIZES.SMALL}>
+        <p>{buttonLabel}</p>
+      </Button>
     </div>
   );
 };
--- src/components/category-card/dto.ts
+++ src/components/category-card/dto.ts
@@ -2,6 +2,7 @@ type CategoryCardProps = {
   title: string;
   totalItems: number;
   totalItemsLabel?: string;
+  buttonLabel?: string;
   additionalStyle?: React.CSSProperties;
 };
 
