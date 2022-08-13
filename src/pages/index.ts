2c9b394a2243cbb5bd2a0bd4af0815e5a2192d5c
Omar Baldi <omarbaldidk@gmail.com>
2022-08-13 17:04:12 +0200

    Created index file for exporting pages to facilitate import across different areas

---
+++ src/pages/index.ts
@@ -0,0 +1,4 @@
+export { BasketPage } from './basket-page';
+export { CategoriesPage } from './categories-page';
+export { CategoryPage } from './category-page';
+export { ProductPage } from './product-page';
