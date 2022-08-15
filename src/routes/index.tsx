d213e05ee462f0fc418628766b7c921176c4f734
Omar Baldi <omarbaldidk@gmail.com>
2022-08-15 13:59:51 +0200

    Handled default entrypoint route and not-existing app route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -11,11 +11,14 @@ export enum ROUTES_PATH {
   CATEGORIES = '/categories',
   SPECIFIC_CATEGORY = '/categories/:category',
   BASKET = '/basket',
+  NOT_FOUND = '*',
 }
 
 export const routes = [
+  { path: ROUTES_PATH.HOME, component: ProductPage },
   { path: ROUTES_PATH.PRODUCTS, component: ProductPage },
   { path: ROUTES_PATH.CATEGORIES, component: CategoriesPage },
   { path: ROUTES_PATH.SPECIFIC_CATEGORY, component: CategoryPage },
   { path: ROUTES_PATH.BASKET, component: BasketPage },
+  { path: ROUTES_PATH.NOT_FOUND, component: ProductPage },
 ];
